5a8759ea1ac4566f403f6ea1667e2203878d6144
git2rails <sungryul@sungryul-ui-MacBook-Air.local>
2013-11-02 22:35:02 +0900

    add user validate

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -3,9 +3,8 @@ class Api::ProfileController < Api::ApiController
   wrap_parameters User
 
   def update
-    puts current_user.to_json
-    current_user.name = params[:user][:name]
-    if current_user.save
+    print user_params
+    if current_user.update(user_params)
       render :json=> {:success=>true}
     else 
       render :json=> current_user.errors, :status=>422
@@ -14,7 +13,7 @@ class Api::ProfileController < Api::ApiController
 
   private
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
