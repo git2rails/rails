08a1f50fc0aa2d531eab02af55d8d83d57f4bf4d
nbp <nbp@nbpui-MacBook-Air.local>
2013-11-11 00:02:23 +0900

    commit

--- app/controllers/api/app_rankings_controller.rb
+++ app/controllers/api/app_rankings_controller.rb
@@ -7,14 +7,14 @@ class Api::AppRankingsController < Api::ApiController
   end
 
   def paid
-    @app_rankings = AppRanking.top_selling_paid.where(paginate(page: params[:page], per_page: 20).order('id DESC')
+    @app_rankings = AppRanking.top_selling_paid.where(paginate(page: params[:page], per_page: 20)).order('id DESC')
     respond_to do |format|
       format.json { render json: to_json(200, "OK", {posts: @app_rankings.to_json(:include => { :app => { :only => :name } })}), status: 200 }
     end
   end
 
   def free
-    @app_rankings = AppRanking.top_selling_free.where(paginate(page: params[:page], per_page: 20).order('id DESC')
+    @app_rankings = AppRanking.top_selling_free.where(paginate(page: params[:page], per_page: 20)).order('id DESC')
     respond_to do |format|
       format.json { render json: to_json(200, "OK", {posts: @app_rankings.to_json(:include => { :app => { :only => :name } })}), status: 200 }
     end
